ac32f7addaab854ca78b07c63183d47015c7d7c5
petak5 <peterurgos12345@gmail.com>
2021-10-29 20:06:08 +0200

    Client cpp hello world

---
+++ src/cpp/main.cpp
@@ -0,0 +1,10 @@
+// ISA project - client implementing a protocol
+// Peter Urgoš (xurgos00)
+// 11/2021
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello, World!" << std::endl;
+}
